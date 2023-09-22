5bbbaea9b4c8e670ae0f1385b3d29d15b8d88c12
lobergo <lorenabergo23@gmail.com>
2023-09-22 15:19:00 -0300

    projeto finalizado

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -2,7 +2,13 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nobis maiores omnis. Commodi, adipisci. Iusto excepturi magni repudiandae dolores eius, quos incidunt laboriosam, quidem, pariatur neque tenetur hic similique eum.
+      Voluptatem tempora voluptatum illum ea blanditiis, dolorem sapiente possimus ratione numquam nam maxime quia aliquid modi expedita ipsa esse nostrum sed! Aliquid molestias perspiciatis libero optio illo impedit ratione quia!
+      Architecto earum expedita non natus laudantium impedit possimus vel eos provident praesentium quod nulla explicabo fuga vitae sequi facere pariatur quo, nisi officia dignissimos. Molestias, eligendi ipsum! Explicabo, aspernatur dignissimos?
+      Enim omnis quas quidem error assumenda recusandae dolore atque numquam laborum voluptatibus. Assumenda, magni! Quas quod impedit nesciunt nam itaque, a ipsam saepe blanditiis natus fugiat praesentium minima illum maxime!</p>
+    </div>
   )
 }
 
